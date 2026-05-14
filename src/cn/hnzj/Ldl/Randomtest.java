a3e49ea5ac4e2983b32357b5e06b19cebcceab28
ldl <3149809235@qq.com>
2026-05-14 11:17:51 +0800

    init project

--- src/cn/hnzj/Ldl/Randomtest.java
+++ src/cn/hnzj/Ldl/Randomtest.java
@@ -4,6 +4,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Randomtest {
+
+    //111
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
