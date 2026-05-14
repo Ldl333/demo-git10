599664e6ee0c51eefeda1f53b650a53891afd7ba
ldl <3149809235@qq.com>
2026-05-14 10:54:37 +0800

    init project

---
+++ src/cn/hnzj/Ldl/Randomtest.java
@@ -0,0 +1,31 @@
+package cn.hnzj.Ldl;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Randomtest {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("请输入要猜的数字：");
+        Random r = new Random();
+
+        int b = r.nextInt(100);
+        while(true){
+            int a = sc.nextInt();
+            if(a>b){
+                System.out.println("偏大了");
+            }
+           if (a<b){
+               System.out.println("偏小了");
+           }
+           if (a==b){
+               System.out.println("猜中了");
+               break;
+           }
+
+        }
+
+    }
+
+}
