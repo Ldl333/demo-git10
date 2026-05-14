a8440f561060f6bf70d22cd65a207e345a47cb76
ldl <3149809235@qq.com>
2026-05-14 12:00:25 +0800

    init project

--- src/cn/hnzj/Ldl/Randomtest.java
+++ src/cn/hnzj/Ldl/Randomtest.java
@@ -18,7 +18,7 @@ public class Randomtest
         while(true){
             int a = sc.nextInt();
             if(a>b){
-                System.out.println("偏大了");
+                System.out.println("篇中了");
             }
            if (a<b){
                System.out.println("偏小了");
